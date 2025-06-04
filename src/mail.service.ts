d3b17c7b69ebcebaac1a0dc12f683527cb7a8af7
Dominic Ingram <dominic@careville.de>
2025-06-04 14:57:30 +0200

    feat: add attachments to send mail

--- src/mail.service.ts
+++ src/mail.service.ts
@@ -1,11 +1,22 @@
 import { ClientSecretCredential } from "@azure/identity";
 import { Client } from "@microsoft/microsoft-graph-client";
 import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
+import { FileAttachment, Message } from "@microsoft/microsoft-graph-types";
 import { Inject, Injectable, Logger } from "@nestjs/common";
 import { render } from "@react-email/components";
 import * as nodemailer from "nodemailer";
 import React from "react";
 import { MODULE_OPTIONS_TOKEN, OptionType } from "./mail.configuration";
+
+export interface MailAttachment {
+  filename: string;
+  content: Buffer | string;
+  contentType?: string;
+}
+
+interface GraphSendMailRequest {
+  message: Message;
+}
 
 @Injectable()
 export class MailService {
@@ -13,18 +24,18 @@ export class MailService {
 
   constructor(@Inject(MODULE_OPTIONS_TOKEN) private readonly options: OptionType) {}
 
-  async sendMail(to: string, subject: string, content: React.ReactElement) {
+  async sendMail(to: string, subject: string, content: React.ReactElement, attachments?: MailAttachment[]) {
     if (this.options.type === "SMTP") {
-      await this.sendMailSMTP(to, subject, content);
+      await this.sendMailSMTP(to, subject, content, attachments);
     } else if (this.options.type === "MS_GRAPH") {
-      await this.sendMailMSGraph(to, subject, content);
+      await this.sendMailMSGraph(to, subject, content, attachments);
     } else {
       this.logger.error("No valid API type specified");
       throw new Error("No valid API type specified");
     }
   }
 
-  private async sendMailSMTP(to: string, subject: string, content: React.ReactElement) {
+  private async sendMailSMTP(to: string, subject: string, content: React.ReactElement, attachments?: MailAttachment[]) {
     const transporter = nodemailer.createTransport({
       host: this.options.smtp.host,
       port: this.options.smtp.port,
@@ -37,11 +48,16 @@ export class MailService {
 
     const html = await render(content);
 
-    const options = {
+    const options: nodemailer.SendMailOptions = {
       from: `${this.options.sender} <${this.options.from}>`,
       to,
       subject,
       html,
+      attachments: attachments?.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
     };
 
     await transporter.sendMail(options).catch((err) => {
@@ -50,7 +66,7 @@ export class MailService {
     });
   }
 
-  private async sendMailMSGraph(to: string, subject: string, content: React.ReactElement) {
+  private async sendMailMSGraph(to: string, subject: string, content: React.ReactElement, attachments?: MailAttachment[]) {
     const credential = new ClientSecretCredential(
       this.options.msGraph.tenantId,
       this.options.msGraph.clientId,
@@ -65,12 +81,32 @@ export class MailService {
 
     const html = await render(content);
 
+    // https://learn.microsoft.com/en-us/graph/api/message-post-attachments?view=graph-rest-1.0&tabs=http
+    const graphAttachments: FileAttachment[] = await Promise.all(
+      attachments.map(async (att) => {
+        let contentBytes: string;
+        if (att.content) {
+          const buf = typeof att.content === "string" ? Buffer.from(att.content) : att.content;
+          contentBytes = buf.toString("base64");
+        } else {
+          throw new Error(`Attachment ${att.filename} has neither content nor path`);
+        }
+
+        return {
+          "@odata.type": "#microsoft.graph.fileAttachment",
+          name: att.filename,
+          contentType: att.contentType,
+          contentBytes,
+        };
+      }),
+    );
+
     const sendMail = async () => {
-      const mail = {
+      const mail: GraphSendMailRequest = {
         message: {
           subject: subject,
           body: {
-            contentType: "HTML",
+            contentType: "html",
             content: html,
           },
           toRecipients: [
@@ -80,6 +116,7 @@ export class MailService {
               },
             },
           ],
+          attachments: graphAttachments,
         },
       };
 
@@ -90,6 +127,6 @@ export class MailService {
       }
     };
 
-    sendMail();
+    await sendMail();
   }
 }
